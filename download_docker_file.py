877cef137a07867ab4197bb05d7a5c03a35de7f9
elior <elior1001@gmail.com>
2023-03-18 22:27:24 +0200

    refactoring script

--- download_docker_file.py
+++ download_docker_file.py
@@ -3,6 +3,9 @@ import os
 import sys
 from github import Github
 
+# This script scans a github repos and saves the all the docker files from the repo to a dolder name docker_file
+
+
 # Set your GitHub access token here or use an environment variable
 access_token = os.environ.get("ACCESS_TOKEN")
 if access_token is not None:
@@ -11,29 +14,36 @@ else:
     print("Make sure to set and env ACCESS_TOKEN")
     sys.exit(0)
 
+
+def save_into_file(repo,file_content):
+     create_docker_files_folder("docker_files")
+     if file_content.encoding == "base64":
+        with open(f"docker_files/{file_content.name}-{repo.name}", "w") as f:
+            print(f"Saving {file_content.name} From {repo.name}")
+            f.write(file_content.decoded_content.decode("utf-8"))
+
+def create_docker_files_folder(folder_path):
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
 g = Github(access_token)
 
-# for repo in g.get_user().get_repos():
-repo = g.get_repo("elior7557/protfolio-DevOps-application")
-# try:
-repo_content = repo.get_contents("")
-while repo_content:
-    file_content = repo_content.pop(0)
-    if file_content.type == "dir":
-        repo_content.extend(repo.get_contents(file_content.path))
-    else:
-        file_name = file_content.name.lower()
-        if "docker" in file_name and file_name != ".dockerignore":
-            print(file_content.name)
-            if file_content.encoding == "base64":
-                # need to decode again from bytes to utf-8
-                print(file_content.decoded_content.decode("utf-8"))    
-
-# except:
-#         print("Something else went wrong")
-
-
-
+for repo in g.get_user().get_repos():
+    try:
+        repo_content = repo.get_contents("")
+        while repo_content:
+            file_content = repo_content.pop(0)
+            if file_content.type == "dir":
+                repo_content.extend(repo.get_contents(file_content.path))
+            else:
+                file_name = file_content.name.lower()
+                if "dockerfile" in file_name and file_name != ".dockerignore":
+                    save_into_file(repo, file_content)
+                    # if file_content.encoding == "base64":
+                    #     with open(f"docker_files/{file_content.name}-{repo.name}", "w") as f:
+                    #         f.write(file_content.decoded_content.decode("utf-8"))
+    except:
+            print("Something else went wrong")
 
 
 
