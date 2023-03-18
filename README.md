79e912436bcda320ab464ab7542d39dcfc5eacfe
elior <elior1001@gmail.com>
2023-03-18 22:31:47 +0200

    refactoring script

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 this python script scans a github repositry and returns all of the docker file
 inside  this list
+
+# Configuration
+1. Create access token for github to access it api
+2. Set the token as env using the command
+```
+export ACCESS_TOKEN=<your acces token>
+```
--- download_docker_file.py
+++ download_docker_file.py
@@ -5,7 +5,6 @@ from github import Github
 
 # This script scans a github repos and saves the all the docker files from the repo to a dolder name docker_file
 
-
 # Set your GitHub access token here or use an environment variable
 access_token = os.environ.get("ACCESS_TOKEN")
 if access_token is not None:
@@ -13,7 +12,6 @@ if access_token is not None:
 else:
     print("Make sure to set and env ACCESS_TOKEN")
     sys.exit(0)
-
 
 def save_into_file(repo,file_content):
      create_docker_files_folder("docker_files")
@@ -39,10 +37,9 @@ for repo in g.get_user().get_repos():
                 file_name = file_content.name.lower()
                 if "dockerfile" in file_name and file_name != ".dockerignore":
                     save_into_file(repo, file_content)
-                    # if file_content.encoding == "base64":
-                    #     with open(f"docker_files/{file_content.name}-{repo.name}", "w") as f:
-                    #         f.write(file_content.decoded_content.decode("utf-8"))
-    except:
+    except Exception as e:
+            print(e)
+            # usually it gets here beacuse the repository was empty
             print("Something else went wrong")
 
 
